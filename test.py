450e70948ef50eb6537a1c95593ef6282c985d7d
iThurmanMurman <129899404+iThurmanMurman@users.noreply.github.com>
2025-11-17 14:02:54 -0600

    Update test.py Added tests for unit conversions

--- test.py
+++ test.py
@@ -29,3 +29,50 @@ response = socket.recv_json()
 # Print response
 print("Received response from server:")
 print(response)
+
+#%%
+# Setup request for convert_volume
+request_volume = {
+    "service_key":"convert_volume",
+    "data": {
+        "amount": 2,
+        "unit": "tablespoon",
+        "to_metric": True
+        }
+    }
+
+request_weight = {
+    "service_key":"convert_weight",
+    "data": {
+        "amount": 1,
+        "unit": "pound",
+        "to_metric": True
+        }
+    }
+
+request_temp_CtoF = {
+    "service_key":"convert_temp",
+    "data": {
+        "value": 180,
+        "direction": "C_to_F"
+        }
+    }
+
+
+def run_test(request_dict):
+    context = zmq.Context()
+    print('Client attempting to connect to server...')
+    socket = context.socket(zmq.REQ)
+    
+    socket.connect("tcp://localhost:5556")
+    print('sending request...')
+    socket.send_json(request_dict)
+    response = socket.recv_json()
+    print("Received response: ")
+    print(response)
+    
+    
+    
+run_test(request_volume)
+run_test(request_weight)
+run_test(request_temp_CtoF)
